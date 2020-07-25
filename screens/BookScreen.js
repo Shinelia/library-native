389440eb85d8508f6155e6284c2d5146221b716c
Shinelia <a.wech62@gmail.com>
2020-07-25 20:15:31 +0200

    displaying book

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
 import { Image, Card} from 'react-native-elements';
+import { Rating, AirbnbRating } from 'react-native-ratings';
 
 
 const BookScreen = ({ navigation, route }) => {
 
   return (
-    <View style={styles.container}>
-      <Card
-        title={route.params.title}>
-        <Text>{route.params.author}</Text>
-
-        <Image 
-        style={styles.img}
-        source={{ uri: route.params.image }}/>
-        <Text style={styles.description}>
-          {route.params.description}
-        </Text>     
-      </Card>
-
-    </View>
+    <ScrollView style={styles.container}>
+        <Card 
+          title={route.params.volumeInfo.title} titleStyle={styles.title} containerStyle={styles.card}>
+          <Text style={styles.section}>Author: {(route.params.volumeInfo.authors) ? (route.params.volumeInfo.authors[0]) : " "}</Text>
+          <Text style={styles.section}>Categories: {route.params.volumeInfo.categories}</Text>
+          <Text style={styles.section}>Published Date: {route.params.volumeInfo.publishedDate}</Text>
+          <Text style={styles.section}>Publisher: {route.params.volumeInfo.publisher}</Text>
+          <Image 
+          style={styles.img}
+          source={{ uri: route.params.volumeInfo.imageLinks.thumbnail}}
+          transition={true}/>
+          <Text style={styles.description}>{route.params.volumeInfo.description}</Text>     
+        </Card>
+    </ScrollView>
   );
 }
 
@@ -30,21 +31,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 10
+    padding: 10,
+  },
+
+  card: {
+    borderColor: "#fa4e4e",
+    borderRadius: 10,
+    marginBottom: 20
+
+  },
+
+  title: {
+    fontSize: 20,
+    color: '#fa4e4e'
+  },
+
+  section: {
+    fontSize: 16,
+    marginBottom: 5
   },
 
   img: {
-    width: 100,
-    height: 150
-  },
-  
-  title: {
-
+    width: 180,
+    height: 250,
+    margin: 50
   },
 
   description: {
-    fontSize: 12,
-    color: 'gray'
-
-  }
+    fontSize: 16,
+    color: 'black',
+    paddingBottom: 30
+  },
 });
